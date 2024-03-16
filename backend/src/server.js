8189a4dd837fbaf064f29a546410be7b70c39ba9
WarutereIan <nmwanik111@gmail.com>
2024-03-16 13:53:19 +0300

    [containerized the backend for easy deployment]

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,7 +42,7 @@ app.use(
     })
   );
 
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 4500);
 app.use(cookieParser());
 
 app.use(`/api/users/`, userRouter);
